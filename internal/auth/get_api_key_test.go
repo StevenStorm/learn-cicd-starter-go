101891dd7a9d6904aef2467ec7200a367b7c5107
István Gulyás <g.istvan91@gmail.com>
2026-06-02 10:46:52 +0200

    Add styling test to ci

--- internal/auth/get_api_key_test.go
+++ internal/auth/get_api_key_test.go
@@ -7,32 +7,32 @@ import (
 
 func TestGetAPIKey(t *testing.T) {
 	tests := map[string]struct {
-		authHeader 		string
-		expectedAPIKey 	string
-		expectError    	error
+		authHeader     string
+		expectedAPIKey string
+		expectError    error
 	}{
 		"no auth header": {
-			authHeader: "",
+			authHeader:     "",
 			expectedAPIKey: "",
 			expectError:    ErrNoAuthHeaderIncluded,
 		},
 		"malformed auth header": {
-			authHeader: "111111",
+			authHeader:     "111111",
 			expectedAPIKey: "",
 			expectError:    ErrMalformedAuthHeader,
 		},
 		"invalid api key format": {
-			authHeader: "11111 22222",
+			authHeader:     "11111 22222",
 			expectedAPIKey: "",
 			expectError:    ErrMalformedAuthHeader,
 		},
 		"valid api key": {
-			authHeader: "ApiKey 11111",
+			authHeader:     "ApiKey 11111",
 			expectedAPIKey: "11111",
 			expectError:    nil,
 		},
 	}
-	
+
 	for name, test := range tests {
 		t.Run(name, func(t *testing.T) {
 			headers := http.Header{}
